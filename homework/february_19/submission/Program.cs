4c77d10d522cd618decfaffd7215de7740b64859
broderick-testa <btesta@chapman.edu>
2026-03-03 17:09:13 -0800

    3-3

---
+++ homework/february_19/submission/Program.cs
@@ -0,0 +1,43 @@
+﻿//set operator
+Console.WriteLine("This calculator can perform 4 operations: addition, subtraction, multiplication, and division.");
+Console.WriteLine("What operation would you like to perform?");
+string operation = Console.ReadLine();
+
+//incorrect submission
+if (!(operation == "addition" || operation == "subtraction" || operation == "multiplication" || operation == "division"))
+{
+    Console.WriteLine("I dont know how to do that");
+    return;
+}
+
+//set numbers
+Console.WriteLine("What is your first number?");
+int a =  int.Parse(Console.ReadLine());
+
+Console.WriteLine("What is your second number?");
+int b = int.Parse(Console.ReadLine());
+
+//divide by zero
+if (operation == "division" && b == 0)
+{
+    Console.WriteLine("You cannot divide by zero");
+    return;
+}
+
+//calculations
+if (operation == "addition")
+{
+    Console.WriteLine(a + " + " + b + " = " + (a + b));
+}
+else if (operation == "subtraction")
+{
+    Console.WriteLine(a + " - " + b + " = " + (a - b));
+}
+else if (operation == "multiplication")
+{
+    Console.WriteLine(a + " * " + b + " = " + (a * b));
+}
+else if (operation == "division")
+{
+    Console.WriteLine(a + " / " + b + " = " + (a / b));
+}
